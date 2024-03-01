5cc069addb2c4697dfb26c9686df205ebab8ae1e
alexander-azizi-martin <alexander.azizi.martin@gmail.com>
2024-03-01 12:41:19 -0500

    Update analysis view

--- lab6/index.js
+++ lab6/index.js
@@ -19,7 +19,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, _, next) => {
-  console.log(`Received request ${req.method} ${req.url}`);
+  console.log(`${req.method} ${req.url}`);
 
   next();
 });
