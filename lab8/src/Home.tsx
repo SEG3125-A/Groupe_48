82974babddf18c03e3b1c60ac22758fc7086d905
alexander-azizi-martin <alexander.azizi.martin@gmail.com>
2024-03-13 23:12:34 -0400

    Add Home & Rome page

---
+++ lab8/src/Home.tsx
@@ -0,0 +1,73 @@
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Heading,
+  Link,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+
+function Home() {
+  return (
+    <Box bgColor={"#ebebeb"} w={"100%"} h={"100%"} pt={4}>
+      <Container maxW={"1000px"} h={"100%"}>
+        <Flex justify={"flex-end"}>
+          <HStack gap={8}>
+            <Link textDecor={"underline"}>About</Link>
+            <Link textDecor={"underline"}>Contact Us</Link>
+            <Link textDecor={"underline"}>How To Use</Link>
+          </HStack>
+        </Flex>
+
+        <Flex pt={8} h={"100%"} align={"start"}>
+          <VStack flex={1} justify={"center"} gap={4}>
+            <Heading as={"h1"} size={"3xl"} color={"#f054b0"}>
+              INSTACHAT
+            </Heading>
+            <Heading as={"h2"} size={"2xl"} color={"#737373"}>
+              Create a Room
+            </Heading>
+            <Button
+              bgColor={"white"}
+              size={"lg"}
+              width={"300px"}
+              borderRadius={"24px"}
+            >
+              Room Name
+            </Button>
+            <Button
+              bgColor={"white"}
+              size={"lg"}
+              width={"300px"}
+              borderRadius={"24px"}
+            >
+              Passcode
+            </Button>
+            <Button
+              bgColor={"white"}
+              size={"lg"}
+              width={"300px"}
+              borderRadius={"24px"}
+            >
+              Nickname
+            </Button>
+            <Button bgColor={"#c1ff72"} size={"lg"} borderRadius={"24px"}>
+              CREATE or JOIN
+            </Button>
+            <Text width={"300px"} textAlign={"center"}>
+              By creating an account, you agree to our{" "}
+              <Link textDecor={"underline"}>Terms of Service</Link>
+            </Text>
+          </VStack>
+
+          <Box bgColor={"white"} width={"400px"} height={"400px"}></Box>
+        </Flex>
+      </Container>
+    </Box>
+  );
+}
+
+export default Home;
---
+++ lab8/src/main.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Room from "./Room.tsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <Room />
+    </ChakraProvider>
+  </React.StrictMode>
+);
